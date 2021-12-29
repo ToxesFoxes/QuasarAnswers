b7054b4a9db5212fce9242ab13c490a2420c0c95
ToxesFoxes <toxes_foxes@outlook.com>
2021-12-29 23:35:45 +0600

    Config test 1

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,10 +47,10 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      publicPath: '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
@@ -109,8 +109,8 @@ module.exports = configure(function (ctx) {
     ssr: {
       pwa: true,
 
-      // manualStoreHydration: true,
-      // manualPostHydrationTrigger: true,
+      manualStoreHydration: true,
+      manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
                       // (gets superseded if process.env.PORT is specified at runtime)
